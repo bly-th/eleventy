99bfb4fb9ff055e53cd971dd2019622352d0d0be
ryangittings <ryan@madebykingfisher.co.uk>
2022-01-20 10:18:18 +0000

    Update styling method

--- .eleventy.js
+++ .eleventy.js
@@ -5,32 +5,32 @@ const pluginRss = require('@11ty/eleventy-plugin-rss');
 // Import filters
 const cssMinFilter = require('./src/filters/css-min.js');
 
-module.exports = (config) => {
+module.exports = (eleventyConfig) => {
   // Add plugins
-  config.addPlugin(eleventyNavigationPlugin);
-  config.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(pluginRss);
 
   // Add filters
-  config.addFilter('cssmin', cssMinFilter);
+  eleventyConfig.addFilter('cssmin', cssMinFilter);
 
   // Pass through
-  config.addPassthroughCopy('./src/img/');
-  config.addPassthroughCopy('./src/fonts/');
-  config.addPassthroughCopy('./src/js/');
-  config.addPassthroughCopy('./src/apple-touch-icon-57x57.png');
-  config.addPassthroughCopy('./src/apple-touch-icon-72x72.png');
-  config.addPassthroughCopy('./src/apple-touch-icon-76x76.png');
-  config.addPassthroughCopy('./src/apple-touch-icon-114x114.png');
-  config.addPassthroughCopy('./src/apple-touch-icon-152x152.png');
-  config.addPassthroughCopy('./src/apple-touch-icon-180x180.png');
-  config.addPassthroughCopy('./src/apple-touch-icon.png');
-  config.addPassthroughCopy('./src/favicon.ico');
-  config.addPassthroughCopy('./src/robots.txt');
+  eleventyConfig.addPassthroughCopy('./src/img/');
+  eleventyConfig.addPassthroughCopy('./src/fonts/');
+  eleventyConfig.addPassthroughCopy('./src/js/');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon-57x57.png');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon-72x72.png');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon-76x76.png');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon-114x114.png');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon-152x152.png');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon-180x180.png');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
+  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('./src/robots.txt');
 
   return {
     dir: {
       input: 'src',
-      output: 'dist',
+      output: '_site',
     },
     passthroughFileCopy: true,
   };
