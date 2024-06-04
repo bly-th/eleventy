2034614f3935ec82a2bf6b8c1bfca458896975d9
ryangittings <ryan@madebykingfisher.co.uk>
2024-06-04 11:02:43 +0100

    Updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 // Import plugins
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 // Import filters
 const cssMinFilter = require('./src/filters/css-min.js');
@@ -12,6 +13,26 @@ module.exports = (eleventyConfig) => {
   // Add plugins
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(pluginRss);
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// optional, output image formats
+		formats: ["webp", "jpeg", "svg"],
+    urlPath: '/img/built/',
+    outputDir: '_site/img/built',
+    svgShortCircuit: true,
+    fixOrientation: true,
+    svgCompressionSize: "br",
+    cacheOptions: {
+      duration: "*",
+      directory: ".cache",
+      removeUrlQueryParams: false,
+    },
+		defaultAttributes: {
+			loading: "lazy",
+      decoding: "async",
+      sizes: '(min-width: 30em) 50vw, 100vw',
+      class: 'w-full h-auto'
+		},
+	});
 
   // Add filters
   eleventyConfig.addFilter('cssmin', cssMinFilter);
