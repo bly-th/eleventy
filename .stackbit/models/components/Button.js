c09612dfec74113c87dbaa3763f5c52627bca10a
Ryan Gittings <ryan@gittings.studio>
2025-02-17 10:30:12 +0000

    Remove stackbit

--- .stackbit/models/components/Button.js
+++
@@ -1,8 +0,0 @@
-export const Button = {
-    name: 'button',
-    type: 'object',
-    fields: [
-        { type: 'string', name: 'text', default: 'Click Me', required: true },
-        { type: 'string', name: 'url', label: 'URL', default: '/', required: true }
-    ]
-};
